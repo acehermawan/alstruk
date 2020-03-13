2f660845d50938746cce5df7e4441ac0eb22ab29
acehermawan <62134881+acehermawan@users.noreply.github.com>
2020-03-13 16:10:36 +0700

    asign.c

---
+++ asign.c
@@ -0,0 +1,16 @@
+/* file : asign.c*/
+#include <stdio.h>
+
+int main()
+{
+	/* kamus */
+	int i ;
+	
+	/* program */
+	printf ("Selamat Siang!!!\n");
+	printf("nama saya : Ace Hermawan\n");
+	i =18 ;
+	printf("usia saya : %d tahun", i);
+	
+	return 0;
+}
